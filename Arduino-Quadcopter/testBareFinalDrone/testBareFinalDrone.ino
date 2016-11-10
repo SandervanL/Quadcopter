f3aa890a6d24f82b76baadb4ddbb665d70f3c7e7
SandervanL <sannievanl@gmail.com>
2016-11-10 17:48:10 +0100

    Remote control in seperate .h file

--- Arduino-Quadcopter/testBareFinalDrone/testBareFinalDrone.ino
+++ Arduino-Quadcopter/testBareFinalDrone/testBareFinalDrone.ino
@@ -5,12 +5,7 @@
 #include <MoveMeter.h>        //Self-written class for the pitch and roll (depends on MPU6050 and AK8975)
 #include <Calculator.h>       //Library for PID calculations
 #include <Motors.h>           //Library for output to motors
-
-
-//Radio control variables, micros() returns a uint32_t
-volatile uint16_t signals[8];   //Array which contains the signals of the remote control in microseconds (updated by ISR(PCINT_vect))
-volatile uint32_t signalBegin[8] = {0};        //Array with beginvalues of the remote-control signals
-volatile uint32_t currentMicroTime;      //Variable used by ISR(PCINT0_vect) to store the current time in
+#include <RemoteControl.h>    //Library for remote control
 
 //Chip, calculation and motor variables
 Calculator    calculator;           //Instance of the PID library
@@ -51,20 +46,7 @@ void setup() {
   motors.initialize();
   moveMeter.initialize();
   calculator.resetPitchRollParameters();
-
-  //Enable the pull-down resistor for better values
-  pinMode(A0, INPUT_PULLUP);
-  pinMode(A1, INPUT_PULLUP);
-  pinMode(A2, INPUT_PULLUP);
-  pinMode(A3, INPUT_PULLUP);
-
-  //Check https://www.arduino.cc/en/Reference/PortManipulation for more information
-  //Setup the remote-control pins
-  PCICR  |= (1 << PCIE1);        //Setup interrupt listening for the analog pins
-  PCMSK1 |= (1 << PCINT8);              //Setup PCINT8 (A0) to trigger an interrupt.
-  PCMSK1 |= (1 << PCINT9);              //Setup PCINT9 (A1) to trigger an interrupt.
-  PCMSK1 |= (1 << PCINT10);             //Setup PCINT10(A2) to trigger an interrupt.
-  PCMSK1 |= (1 << PCINT11);             //Setup PCINT11(A3) to trigger an interrupt.
+  initializeRemoteControl();
 };
 
 
@@ -133,43 +115,3 @@ void loop() {
   Serial.println(startState);
 #endif
 };
-
-ISR (PCINT1_vect) {
-  currentMicroTime = micros();
-  //Channel 1
-  if ((PINC & B00000001) ) {              //Is input 8 high?
-    if (signalBegin[0] == 0) {                    //Input 8 changed from 0 to 1
-      signalBegin[0] = currentMicroTime;      //Remember the beginning of the pulse
-    };
-  } else if (signalBegin[0]) {               //Input 8 is not high and changed from 1 to 0
-    signals[0] = currentMicroTime - signalBegin[0]; //Channel 1 is current time - begin time
-    signalBegin[0] = 0;                       //Remember channel 1 is now low
-  };
-  //Channel 2
-  if ((PINC & B00000010) ) {              //Is input 9 high?
-    if (signalBegin[1] == 0) {                    //Input 9 changed from 0 to 1
-      signalBegin[1] = currentMicroTime;      //Remember the beginning of the pulse
-    };
-  } else if (signalBegin[1]) {                //Input 9 is not high and changed from 1 to 0
-    signals[1] = currentMicroTime - signalBegin[1];//Channel 2 is current time - begin time
-    signalBegin[1] = 0;                       //Remember channel 2 is now low
-  };
-  //Channel 3
-  if ((PINC & B00000100) ) {              //Is input 10 high?
-    if (signalBegin[2] == 0) {                    //Input 10 changed from 0 to 1
-      signalBegin[2] = currentMicroTime;      //Remember the beginning of the pulse
-    };
-  } else if (signalBegin[2]) {                //Input 10 is not high and changed from 1 to 0
-    signals[2] = currentMicroTime - signalBegin[2];//Channel 3 is current time - begin time
-    signalBegin[2] = 0;                       //Remember channel 3 is now low
-  };
-  //Channel 4
-  if ((PINC & B00001000) ) {              //Is input 11 high?
-    if (signalBegin[3] == 0) {                    //Input 11 changed from 0 to 1
-      signalBegin[3] = currentMicroTime;      //Remember the beginning of the pulse
-    };
-  } else if (signalBegin[3]) {                 //Input 11 is not high and changed from 1 to 0
-    signals[3] = currentMicroTime - signalBegin[3];//Channel 4 is current time - begin time
-    signalBegin[3] = 0;                       //Remember channel 4 is now low
-  };
-}
--- Arduino101-Quadcopter/testBareFinalDrone/testBareFinalDrone.ino
+++ Arduino101-Quadcopter/testBareFinalDrone/testBareFinalDrone.ino
@@ -2,10 +2,7 @@
 #include <MoveMeter.h>        //Self-written class for the pitch and roll (depends on CurieImu)
 #include <Calculator.h>       //Library for PID calculations
 #include <Motors.h>           //Library for output to motors
-
-//Radio control variables
-volatile uint32_t signals[8];     //Array which contains the signals of the remote control in microseconds (updated by ISR(PCINT_vect))
-volatile uint32_t signalBegin[8]; //Array with beginvalues of the remote-control signals
+#include <RemoteControl.h>
 
 //Chip, calculation and motor variables
 Calculator    calculator;         //Instance of the PID library
@@ -25,7 +22,7 @@ byte timePortState = 0; //An even portState indicates on, odd indicates off.
 
 void setup() {
 #ifdef DEBUG
-  Serial.begin(9600);                       //Initialize serial bus if we want to debug our code
+  Serial.begin(115200);                       //Initialize serial bus if we want to debug our code
   while (!Serial);
   Serial.println("Online!");
   pinMode(timePort, OUTPUT);
@@ -53,22 +50,7 @@ void setup() {
   motors.initialize();
   moveMeter.initialize();
   calculator.resetAll();
-
-  //Enable the pull-up resistor for better values
-  pinMode(A0, INPUT_PULLUP);
-  pinMode(A1, INPUT_PULLUP);
-  pinMode(A2, INPUT_PULLUP);
-  pinMode(A3, INPUT_PULLUP);
-  pinMode(A4, INPUT_PULLUP);
-  pinMode(A5, INPUT_PULLUP);
-
-  //Setup the interrupts to watch the remote-control values change
-  attachInterrupt(A0, A0Interrupt, CHANGE);
-  attachInterrupt(A1, A1Interrupt, CHANGE);
-  attachInterrupt(A2, A2Interrupt, CHANGE);
-  attachInterrupt(A3, A3Interrupt, CHANGE);
-  attachInterrupt(A4, A4Interrupt, CHANGE);
-  attachInterrupt(A5, A5Interrupt, CHANGE);
+  initializeRemoteControl();
 };
 
 
@@ -147,57 +129,3 @@ void loop() {
   Serial.println(startState);
 #endif
 };
-
-void A0Interrupt() {
-  if (!signalBegin[0]) {
-    signalBegin[0] = micros();
-  } else {
-    signals[0] = micros() - signalBegin[0];
-    signalBegin[0] = 0;
-  }
-}
-
-void A1Interrupt() {
-  if (!signalBegin[1]) {
-    signalBegin[1] = micros();
-  } else {
-    signals[1] = micros() - signalBegin[1];
-    signalBegin[1] = 0;
-  }
-}
-
-void A2Interrupt() {
-  if (!signalBegin[2]) {
-    signalBegin[2] = micros();
-  } else {
-    signals[2] = micros() - signalBegin[2];
-    signalBegin[2] = 0;
-  }
-}
-
-void A3Interrupt() {
-  if (!signalBegin[3]) {
-    signalBegin[3] = micros();
-  } else {
-    signals[3] = micros() - signalBegin[3];
-    signalBegin[3] = 0;
-  }
-}
-
-void A4Interrupt() {
-  if (!signalBegin[4]) {
-    signalBegin[4] = micros();
-  } else {
-    signals[4] = micros() - signalBegin[4];
-    signalBegin[4] = 0;
-  }
-}
-
-void A5Interrupt() {
-  if (!signalBegin[5]) {
-    signalBegin[5] = micros();
-  } else {
-    signals[5] = micros() - signalBegin[5];
-    signalBegin[5] = 0;
-  }
-}
